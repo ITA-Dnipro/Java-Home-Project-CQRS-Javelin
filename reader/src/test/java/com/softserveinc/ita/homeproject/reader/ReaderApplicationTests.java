983a64d511dd7078aee97ef19236fe3d2a41c6cd
vladyslavyarets <zzzlpzzz0@gmail.com>
2022-05-14 01:46:12 +0300

    Initial commit after creating module with Spring Initializr

---
+++ reader/src/test/java/com/softserveinc/ita/homeproject/reader/ReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserveinc.ita.homeproject.reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
