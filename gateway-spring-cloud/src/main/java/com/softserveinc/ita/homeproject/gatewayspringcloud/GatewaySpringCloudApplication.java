aa4616aaf34441e6f68b0244ee6aa8e98532cc91
PaulNB <paulspam@ukr.net>
2022-05-20 11:20:18 +0300

    Fixed after review

--- gateway-spring-cloud/src/main/java/com/softserveinc/ita/homeproject/gatewayspringcloud/GatewaySpringCloudApplication.java
+++ gateway-spring-cloud/src/main/java/com/softserveinc/ita/homeproject/gatewayspringcloud/GatewaySpringCloudApplication.java
@@ -3,6 +3,7 @@ package com.softserveinc.ita.homeproject.gatewayspringcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GatewaySpringCloudApplication {
 
