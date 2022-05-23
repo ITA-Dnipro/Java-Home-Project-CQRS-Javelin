3e17de4e8971aed3ba923d850871d6afd594fc34
Artem <puhachofficial@gmail.com>
2022-05-23 13:17:16 +0300

    Modified gateway configuration with java annotations declaration

--- gateway-spring-cloud/src/main/java/com/softserveinc/ita/homeproject/gatewayspringcloud/GatewaySpringCloudApplication.java
+++ gateway-spring-cloud/src/main/java/com/softserveinc/ita/homeproject/gatewayspringcloud/GatewaySpringCloudApplication.java
@@ -2,6 +2,10 @@ package com.softserveinc.ita.homeproject.gatewayspringcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
 
 @SpringBootApplication
@@ -9,5 +13,23 @@ public class GatewaySpringCloudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewaySpringCloudApplication.class, args);
+    }
+
+    /**
+     * Used environment variable for reader service access.
+     * Example:
+     * http://localhost:8085 - for running from IDEA
+     * http://host.docker.internal:8085 - for running from Docker
+     * @param builder
+     * @return routes with which Gateway would interact with other services
+     */
+    @Bean
+    public RouteLocator getRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route(r -> r.method(HttpMethod.GET)
+                .uri("http://localhost:8085/api/0/**"))
+            .route(r -> r.method(HttpMethod.POST, HttpMethod.PUT, HttpMethod.PATCH, HttpMethod.DELETE)
+                .uri("http://localhost:8086/api/0/**"))
+            .build();
     }
 }
