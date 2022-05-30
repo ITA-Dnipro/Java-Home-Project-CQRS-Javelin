2a452b641b611be3e578d57540d0229bea8cbc6a
PaulNB <paulspam@ukr.net>
2022-05-30 11:08:43 +0300

    Fixing review questions

--- eureka-discovery-service/src/main/java/com/softserveinc/ita/homeproject/eurekadiscoveryservice/EurekaDiscoveryServiceApplication.java
+++ eureka-discovery-service/src/main/java/com/softserveinc/ita/homeproject/eurekadiscoveryservice/EurekaDiscoveryServiceApplication.java
@@ -11,5 +11,4 @@ public class EurekaDiscoveryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaDiscoveryServiceApplication.class, args);
     }
-
 }
